fa8dac14ac3548cbbf78bdcc86b806e9ae07e46e
DanAstakhov <astakhov97@gmail.com>
2022-12-05 21:14:28 -0500

    Assignment done!

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function getCatImage() {
-    fetch("https://api.thecatapi.com/v1/images/search")
-        .then((response) => {
-            // Resolve the promise we got from calling then() to the promise that we get from calling json()
-            response.json();
-        })
-        .then((data) => {
-            console.log(data[0].url);
-            document.getElementById("catImage").src = data[0].url;
-        });
+  const divImgContainer = document.getElementById("image-container");
+
+  const createImgElement = document.createElement("img");
+
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      createImgElement.src = data[0].url;
+      console.log(data[0].url);
+      divImgContainer.appendChild(createImgElement);
+    });
 }
